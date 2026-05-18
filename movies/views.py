9c248584f53b42b32deba8e2455f573b5076865c
HtetMyoAung <htetmyoaung37@gmail.com>
2026-05-19 00:31:54 +0900

    connect to the template and view files that were recently edited. The commit message should reflect the changes made in these files.

--- movies/views.py
+++ movies/views.py
@@ -6,6 +6,6 @@ from .models import Movie, Genre
 # say_hello ဆိုတဲ့ function တစ်ခု ဆောက်လိုက်တာပါ (နာမည်ကြိုက်တာပေးလို့ရတယ်)
 def say_hello(request):
     movies = Movie.objects.all()  # Movie model ထဲက အားလုံးကို ရယူပါ
-    # ရုပ်ရှင်နာမည်တွေကို comma နဲ့ ခွဲပြီး string ပြောင်းပါ
-    output = ', '.join([movie.title for movie in movies])
-    return HttpResponse(output)  # HTTP response အဖြစ် ရုပ်ရှင်
+    genres = Genre.objects.all()  # Genre model ထဲက အားလုံးကို ရယူပါ
+    # movies/index.html ကို render လုပ်ပြီး movies နဲ့ genres ကို context အဖြစ်ပေးပါ
+    return render(request, 'movies/index.html', {'movies': movies, 'genres': genres})
