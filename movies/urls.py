ef43bb1912b0ba627d1c133f590728397dee72a6
HtetMyoAung <htetmyoaung37@gmail.com>
2026-05-19 21:18:10 +0900

    add url parameters to view and  create detail.html

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,5 @@ from . import views  # လက်ရှိ folder ထဲက views.py ကို �
 urlpatterns = [
     # path က အလွတ်ထားရပါမယ်။ ဘာလို့လဲဆိုတော့ Project ရဲ့ urls.py ကနေ 'movies/' လို့ လှမ်းခေါ်မှာမို့လို့ပါ
     path('', views.say_hello, name='index'),
+    path('<int:movie_id>', views.movie_detail, name='detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -9,3 +9,10 @@ def say_hello(request):
     genres = Genre.objects.all()  # Genre model ထဲက အားလုံးကို ရယူပါ
     # movies/index.html ကို render လုပ်ပြီး movies နဲ့ genres ကို context အဖြစ်ပေးပါ
     return render(request, 'movies/index.html', {'movies': movies, 'genres': genres})
+
+
+def movie_detail(request, movie_id):
+    # movie_id နဲ့ ကိုက်တဲ့ Movie ကို ရယူပါ
+    movie = Movie.objects.get(id=movie_id)
+    # movies/detail.html ကို render လုပ်ပြီး movie ကို context အဖြစ်ပေးပါ
+    return render(request, 'movies/detail.html', {'movie': movie})
